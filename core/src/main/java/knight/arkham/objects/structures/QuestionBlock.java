d7f27773d47e94ed120361cffa25a696bc8f8f1e
karvin <karvin.jimenez@alternard.com>
2024-04-04 17:58:02 -0400

    added QuestionBlock behaviour.

--- core/src/main/java/knight/arkham/objects/structures/QuestionBlock.java
+++ core/src/main/java/knight/arkham/objects/structures/QuestionBlock.java
@@ -1,6 +1,7 @@
 package knight.arkham.objects.structures;
 
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.World;
@@ -8,9 +9,12 @@ import knight.arkham.helpers.Box2DBody;
 import knight.arkham.helpers.Box2DHelper;
 
 public class QuestionBlock extends InteractiveStructure {
+    private final TiledMapTileSet tileSet;
 
     public QuestionBlock(Rectangle rectangle, World world, TiledMap tiledMap) {
         super(rectangle, world, tiledMap, "coin.wav");
+
+        tileSet = tiledMap.getTileSets().getTileSet("OverWorld");
     }
 
     @Override
@@ -24,6 +28,15 @@ public class QuestionBlock extends InteractiveStructure {
     public void hitByPlayer() {
 
         collisionSound.play();
+
+        int BLANK_COIN = 3;
+
+//        Hay 2 sonidos que podemos tocar, uno cuando hay un coin disponible y otro cuando el bloque esta vació.
+//        Comparo él, id actual del tile si es igual a un Blank_coin significa que el tile esta vació.
+        if(getObjectCellInTheTileMap().getTile().getId() != BLANK_COIN){
+
+            getObjectCellInTheTileMap().setTile(tileSet.getTile(BLANK_COIN));
+        }
 
 //        collisionWithPlayer();
 //
